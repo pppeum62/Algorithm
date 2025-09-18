77e79a66cb8155aeea1d697583282a7bf607a237
이기쁨 <im.thisjoy@gmail.com>
2025-09-18 20:57:31 +0900

    level01.지폐 접기
    https://school.programmers.co.kr/learn/courses/30/lessons/340199

---
+++ programmers/난이도별/level01.지폐_접기/pppeum62.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        Arrays.sort(wallet);
+        Arrays.sort(bill);
+        
+        while (wallet[0] < bill[0] || wallet[1] < bill[1]) {
+            bill[1] /= 2;
+            Arrays.sort(bill);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
